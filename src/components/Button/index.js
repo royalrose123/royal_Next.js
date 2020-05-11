c8491018a488633045a0b768da81c691b92addde
Royal Chen <royal.chen@cloud-interactive.com>
2020-05-11 15:47:24 +0800

    Add Button component

---
+++ src/components/Button/index.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styles from './style.module.scss'
+import classnames from 'classnames/bind'
+
+// components
+
+export const propTypes = {
+  children: PropTypes.node.isRequired,
+  type: PropTypes.string,
+}
+
+export const defaultProps = {
+  type: 'default',
+}
+
+function Button(props) {
+  const cx = classnames.bind(styles)
+
+  const { children, type, ...restProps } = props
+
+  return (
+    <button className={cx('button')} data-type={type} {...restProps}>
+      {children}
+    </button>
+  )
+}
+
+Button.propTypes = propTypes
+Button.defaultProps = defaultProps
+
+export default Button
