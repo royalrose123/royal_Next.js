6580425e32f571fdba713b3f2021dcae8fa04090
Royal Chen <royal.chen@cloud-interactive.com>
2020-05-11 15:47:35 +0800

    Add BookItem component

---
+++ src/components/BookItem/index.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styles from './style.module.scss'
+import classnames from 'classnames/bind'
+import { useRouter } from 'next/router'
+
+// components
+import Button from '@/components/Button'
+
+export const propTypes = {
+  book: PropTypes.object,
+}
+
+function BookItem(props) {
+  const cx = classnames.bind(styles)
+
+  const { book } = props
+  const { id, name, price, rentPrice } = book
+
+  const router = useRouter()
+
+  const onButtonClick = () => {
+    console.log('id', id)
+    router.push(`/home/book/${id}`)
+  }
+
+  return (
+    <div className={cx('book-item')}>
+      <p className={cx('book-item__context')}>id : {id}</p>
+      <p className={cx('book-item__context')}>name : {name}</p>
+      <p className={cx('book-item__context')}>price : {price}</p>
+      <p className={cx('book-item__context')}>rent_price : {rentPrice}</p>
+      <Button onClick={onButtonClick}>Read More</Button>
+    </div>
+  )
+}
+
+BookItem.propTypes = propTypes
+
+export default BookItem
