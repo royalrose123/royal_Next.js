f1831de88fb154836066ee5c89fe51c7057b3bcb
Royal Chen <royal.chen@cloud-interactive.com>
2020-05-28 22:49:05 +0800

    Update BookItem component

--- src/components/BookItem/index.js
+++ src/components/BookItem/index.js
@@ -1,4 +1,3 @@
-import React from 'react'
 import PropTypes from 'prop-types'
 import styles from './style.module.scss'
 import classnames from 'classnames/bind'
@@ -15,7 +14,7 @@ function BookItem(props) {
   const cx = classnames.bind(styles)
 
   const { book } = props
-  const { id, name, price, rentPrice } = book
+  const { id, name, price, rent_price } = book
 
   const router = useRouter()
 
@@ -28,7 +27,7 @@ function BookItem(props) {
       <p className={cx('book-item__context')}>id : {id}</p>
       <p className={cx('book-item__context')}>name : {name}</p>
       <p className={cx('book-item__context')}>price : {price}</p>
-      <p className={cx('book-item__context')}>rent_price : {rentPrice}</p>
+      <p className={cx('book-item__context')}>rent_price : {rent_price}</p>
       <div className={cx('book-item__button')}>
         <Button onClick={onButtonClick}>Read More</Button>
       </div>
