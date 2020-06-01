91c7b6f475a75a95fe52158afba99ecfeff449ea
Royal Chen <royal.chen@cloud-interactive.com>
2020-06-01 10:34:44 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,7 +6,6 @@ module.exports = {
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//))
 
-    // console.log('config 22222', config)
     return config
   },
   webpackDevMiddleware: (config) => {
