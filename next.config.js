d7d7c86cfa19478c29fca27d03972003f407949a
Royal Chen <royal.chen@cloud-interactive.com>
2020-06-01 16:54:08 +0800

    Update env configs

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@ module.exports = {
   env: {
     TEST: process.env.TEST,
     API_KEY: process.env.API_KEY,
+    API_LOCAL_URL: process.env.API_LOCAL_URL,
+    API_DEV_URL: process.env.API_DEV_URL,
+    API_SIT_URL: process.env.API_SIT_URL,
+    API_UAT_URL: process.env.API_UAT_URL,
+    COOKIE: process.env.COOKIE,
+    ENV_KEY: process.env.ENV_KEY,
   },
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//))
